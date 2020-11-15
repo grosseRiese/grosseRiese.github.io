5c0ae1b4bd10046b9a1f26d96fa40405b0c4708d
Grosse Riese <plmman2010@gmail.com>
2020-11-15 17:19:19 +0100

    test
    test something

---
+++ test-file/precache-manifest.f1243b1022430e51ee7a265ac2bec255.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93f263e611ebe670eb40",
+    "url": "css/app.ef97f2e0.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "147751551dc3ff9172db75af3fb3e40c",
+    "url": "index.html"
+  },
+  {
+    "revision": "82f909c0069f80f42601",
+    "url": "js/about.b08aac51.js"
+  },
+  {
+    "revision": "93f263e611ebe670eb40",
+    "url": "js/app.b02e21c0.js"
+  },
+  {
+    "revision": "40d29212c95f70c22630",
+    "url": "js/chunk-vendors.c899bf06.js"
+  },
+  {
+    "revision": "7ba86938a71dfaa44bc5e210e8b7679f",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
