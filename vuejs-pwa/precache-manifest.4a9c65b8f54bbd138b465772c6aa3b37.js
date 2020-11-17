5fa32c7f856e3dda085f9fac502b403976b627c0
Grosse Riese <plmman2010@gmail.com>
2020-11-17 03:35:59 +0100

    add new pws vuejs
    pwa vue

---
+++ vuejs-pwa/precache-manifest.4a9c65b8f54bbd138b465772c6aa3b37.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17fec40089cdf38dddf1",
+    "url": "css/app.1fb07a3a.css"
+  },
+  {
+    "revision": "bcd851cd39e3e034a635318b14cbc85b",
+    "url": "img/images-regular.bcd851cd.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "3983a68a690c5fdbbe73a089fe075b80",
+    "url": "img/play-solid.3983a68a.svg"
+  },
+  {
+    "revision": "22ccaf41f4f787eded6bc43b7380626c",
+    "url": "img/stop-circle-regular.22ccaf41.svg"
+  },
+  {
+    "revision": "3876cffde794a0566ef80480c2a72e40",
+    "url": "index.html"
+  },
+  {
+    "revision": "17fec40089cdf38dddf1",
+    "url": "js/app.2c223e72.js"
+  },
+  {
+    "revision": "a44aac62f87a2fbaa7da",
+    "url": "js/chunk-vendors.65133473.js"
+  },
+  {
+    "revision": "7ba86938a71dfaa44bc5e210e8b7679f",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
