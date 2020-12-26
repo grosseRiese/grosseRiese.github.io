7d3d1fa3846ca4b845a097d5cd09b2a3bddf9eba
Grosse Riese <plmman2010@gmail.com>
2020-12-26 04:43:06 +0100

    Update files
    CRUD operations happend...

--- Instablam/js/service-worker.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js')
-    .then(reg => {
-        console.log('Registration succeeded. Scope is ' + reg.scope);
-    }).catch((error) => {
-        // registration failed
-        console.log('Registration failed with ' + error);
-    });
-}
